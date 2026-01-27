65956fcae4795ae4f4a41f8eb3032cbda500d79d
Been <yubeen721@gmail.com>
2026-01-27 22:06:18 +0800

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',     
-      assets: 'build',    
-      fallback: null
+      fallback: "404.html", 
     }),
- 
     paths: {
-      base: ''
-    }
-  }
+      base: "", 
+    },
+  },
 };
 
 export default config;
-
